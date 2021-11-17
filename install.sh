e15fd9e19d8277f230ba3421474c415ee19bf0e3
madewgn <mad.ewgn2@gmail.com>
2021-11-17 13:18:09 +0800

    push

---
+++ install.sh
@@ -0,0 +1,9 @@
+# bin/bash
+apt update && apt upgrade
+apt install python3
+git clone https://github.com/MADEWGN/suka-nulis
+cd suka-nulis
+
+clear
+
+echo "berhasil di instal ! ketik python3 nulis.py untuk menjalakan nya. www.madewgn.my.id"
